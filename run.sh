a7caa3d3c934b5cd6066935df60e64a3abdd4f55
Divyanshu <divyanshu.info@gmail.com>
2023-11-05 13:19:27 +0000

    05 sep 23

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-git clone https://github.com/Divyanshu-info/Market-Basket-Analysis.git
-sudo apt update
-sudo apt install p7zip-full -y
-7z e ./Market-Basket-Analysis/instacart-market-basket-analysis.7z.001 -o./
+pip3 install kaggle
+cp kaggle.json ~/.kaggle/
+sudo chmod 600 ~/.kaggle/kaggle.json
+kaggle datasets download -d psparks/instacart-market-basket-analysis
+unzip instacart-market-basket-analysis.zip
 sudo fallocate -l 10G ./swapfile
 sudo chmod 600 ./swapfile
 sudo mkswap ./swapfile
